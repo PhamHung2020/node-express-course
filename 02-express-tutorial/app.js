2aa2a90d083c36ac44ee6fc09ccaafc2b2b56e09
PhamHung2020 <phammanhhung10501@gmail.com>
2022-03-18 11:02:54 +0700

    create server using http module

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+const http = require('http');
+const { readFileSync } = require('fs');
+
+const homePageContent = readFileSync('./navbar-app/index.html');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    });
+    if (req.url === '/')
+        res.write(homePageContent)
+    else if (req.url === '/about')
+        res.write('<h1>About page</h1>')
+    else
+        res.write('Not thing')
+    res.end('some thing');
+});
+
+server.listen(5000, () => console.log('Server is running'));
