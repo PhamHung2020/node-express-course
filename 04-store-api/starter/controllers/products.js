3cb2173dd1938391c5a8d1f016d04250d657c9c1
PhamHung2020 <phammanhhung10501@gmail.com>
2022-03-21 14:43:01 +0700

    implement search functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,26 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('error');
-    return res.status(200).json({ message: 'Static'});
+    const products = await Product.find();
+    return res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    return res.status(200).json({ message: 'Products' });
+    const { name, feature, company } = req.query;
+    const queryObject = {};
+    if (feature) {
+        queryObject.feature = feature === 'true' ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i'};
+    }
+    const products = await Product.find(queryObject);
+    return res.status(200).json({ products, nbHits: products.length });
 }
 
 module.exports = {
