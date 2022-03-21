eca41e0a0f023dd0f88ff3bfd83fd3a1893f2dbc
PhamHung2020 <phammanhhung10501@gmail.com>
2022-03-21 08:59:42 +0700

    set up connecting database

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+// async errors
+
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/product">Product Route</a>');
+});
+
+// product route
+
+// error
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+// start server
+const startServer = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`Server is running in port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
