4415f822eba5ae9c9a496d491ca2b41b3ea4820b
PhamHung2020 <phammanhhung10501@gmail.com>
2022-03-22 09:38:26 +0700

    Add main functionality

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,10 +1,45 @@
+
+const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
+
 const login = (req, res) => {
-    res.status(200).send('fake login');
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new CustomAPIError('Username or Password is missing', 400);
+    }
+
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+
+    res.status(200).send({ 
+        message: 'User created',
+        token,
+    });
 }
 
 const dashboard = (req, res) => {
-    const secret = Math.floor(Math.random() * 100);
-    res.status(200).json({message: `Your secret is ${secret}`});
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token provided', 401);
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        throw new CustomAPIError('Token is invalid', 401);
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const secret = Math.floor(Math.random() * 100);
+        res.status(200).json({
+            username: decoded.username, 
+            message: `Your secret is ${secret}`
+        });
+    } catch (error) {
+        throw new CustomAPIError('No authorized info provided', 401);
+    }
 }
 
 module.exports = {
