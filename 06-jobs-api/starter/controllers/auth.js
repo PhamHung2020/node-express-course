047ddc8cd7c4deba6ade25e4590832c2ce5dbaa8
PhamHung2020 <phammanhhung10501@gmail.com>
2022-03-23 10:49:25 +0700

    implement basic login functionality

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,29 +1,54 @@
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
+const passwordUtils = require('../utils/password.utils');
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
     // extract info (name, email, password) from req.body
     const { name, email, password } = req.body;
     // validate the info
-    // ** use mongoose to validate **
-
-    // generate salt and hash password
-    // ** middleware in schema take this responsibility **
-
+    // ** TODO **
+    // hash password
+    const hashedPassword = await passwordUtils.hashPassword(password);
     // create user with the info and hashed password
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password: hashedPassword });
     // generate token from id, name
-    const token = jwt.sign({ id: user._id, name: user.name}, process.env.JWT_SECRET, {
+    const token = jwt.sign({ 
+        id: user._id, 
+        name: user.name
+    }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_LIFETIME
     });
     // return username, token
-    return res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+    return res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
-const login = (req, res) => {
-    res.send('login');
+const login = async (req, res) => {
+    // extract info (email, password) from req.body
+    const { email, password } = req.body;
+    // validate the info - TODO
+    // check if the user with email exist
+    const user = await User.findOne({ email });
+    // if not, return bad request
+    if (!user) {
+        throw new UnauthenticatedError('Invalid credentials');
+    }
+    // compare password
+    const isPasswordCorrect = await passwordUtils.comparePassword(password, user.password);
+    // if password is not correct, return unauthenticated
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid credentials');
+    }
+    // generate token from id, name
+    const token = jwt.sign({ 
+        id: user._id, 
+        name: user.name
+    }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    });
+    // return
+    return res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 }
 
 module.exports = {
--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -21,9 +21,9 @@ const UserSchema = mongoose.Schema({
     },
 });
 
-UserSchema.pre('save', async function() {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
+// UserSchema.pre('save', async function() {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+// });
 
 module.exports = mongoose.model('User', UserSchema);
